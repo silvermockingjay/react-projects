04039b3e92e464facfe5de91666746cb84fbf7a2
anakdrch <ana.kudrevich@gmail.com>
2025-07-13 21:54:21 +0200

    feat: implement App component

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { SearchPage } from './pages/SearchPage';
+import './App.css';
+
+export class App extends React.Component {
+  render() {
+    return <SearchPage />;
+  }
+}
