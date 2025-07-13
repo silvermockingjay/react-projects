25f8cd4e28d32d842168815a3adca2873a4de900
anakdrch <ana.kudrevich@gmail.com>
2025-07-13 13:31:39 +0200

    feat: implement SearchForm component

---
+++ src/components/SearchForm/SearchForm.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button } from '../Button/Button';
+import { Input } from '../Input/Input';
+import './SearchForm.module.css';
+
+interface FormProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+}
+
+export class SearchForm extends React.Component<FormProps> {
+  render() {
+    const { value, onSubmit, onChange } = this.props;
+    return (
+      <form className="search-form" onSubmit={onSubmit}>
+        <Input
+          type="text"
+          value={value}
+          className="search-input"
+          placeholder="Search for character, planet or episode"
+          onChange={onChange}
+        />
+        <Button type="submit" className="search-btn" text="Search" />
+      </form>
+    );
+  }
+}
