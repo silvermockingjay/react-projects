7d48344e91da622691018778c4db4a5e6a44185e
anakdrch <ana.kudrevich@gmail.com>
2025-07-20 19:50:57 +0200

    test: implement unit-test for CardList component

---
+++ src/components/CardList/CardList.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+import type { CardListProps } from './CardList';
+import { CardList } from './CardList';
+
+const mockData: CardListProps = {
+  cards: [
+    {
+      id: 1,
+      name: 'Rick Sanchez',
+      status: 'Alive',
+      species: 'Human',
+      type: '',
+      gender: 'Male',
+      origin: { name: 'Earth', location: 'someLocation' },
+      location: { name: 'Earth', location: 'someLocation' },
+      image: 'someImageURL',
+      episode: ['someEpisode1', 'someEpisode2'],
+      url: 'someURL',
+      created: '2017-11-04T18:48:46.250Z',
+    },
+    {
+      id: 2,
+      name: 'Morty Smith',
+      status: 'Alive',
+      species: 'Human',
+      type: '',
+      gender: 'Male',
+      origin: { name: 'Earth', location: 'someLocation' },
+      location: { name: 'Earth', location: 'someLocation' },
+      image: 'someImageURL2',
+      episode: ['someEpisode1', 'someEpisode2'],
+      url: 'someURL',
+      created: '2017-11-04T18:50:21.651Z',
+    },
+  ],
+};
+
+describe('CardList', () => {
+  it('renders successfully CardList with all props', () => {
+    render(<CardList {...mockData} />);
+    expect(screen.getByText('Rick Sanchez')).toBeInTheDocument();
+    expect(screen.getByText('Morty Smith')).toBeInTheDocument();
+  });
+});
