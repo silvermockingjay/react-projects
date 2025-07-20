bd7dfea0d7c39eae9ba8f35fe3a014fcd592938c
anakdrch <ana.kudrevich@gmail.com>
2025-07-20 19:08:35 +0200

    test: implement unit-test for Card component

---
+++ src/components/Card/Card.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react';
+import { Card } from './Card';
+import type { CardProps } from './Card';
+
+const mockData: CardProps = {
+  image: 'someURL',
+  name: 'John',
+  status: 'alive',
+  species: 'human',
+  gender: 'male',
+  origin: { name: 'Earth', url: 'someURL' },
+  location: { name: 'Earth', url: 'someURL' },
+};
+
+describe('Card', () => {
+  test('renders card successfully with all props', () => {
+    render(<Card {...mockData} />);
+    const list = screen.getByRole('list');
+    const image = screen.getByRole('img');
+    expect(image).toHaveAttribute('src', mockData.image);
+    expect(image).toHaveAttribute('alt', mockData.name);
+    expect(
+      screen.getByRole('heading', { name: mockData.name })
+    ).toBeInTheDocument();
+    expect(screen.getAllByRole('listitem')).toHaveLength(5);
+    expect(list).toHaveTextContent(
+      new RegExp(`Status: ${mockData.status}`, 'i')
+    );
+    expect(list).toHaveTextContent(
+      new RegExp(`Species: ${mockData.species}`, 'i')
+    );
+    expect(list).toHaveTextContent(
+      new RegExp(`Gender: ${mockData.gender}`, 'i')
+    );
+    expect(list).toHaveTextContent(
+      new RegExp(`Origin: ${mockData.origin.name}`, 'i')
+    );
+    expect(list).toHaveTextContent(
+      new RegExp(`Location: ${mockData.location.name}`, 'i')
+    );
+  });
+});
