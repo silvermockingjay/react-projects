3299925f8dc2ef6635e9ddb4104a8e5b7b4c240b
anakdrch <ana.kudrevich@gmail.com>
2025-07-27 22:37:32 +0200

    feat: implement About page

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,24 @@
+export function AboutPage() {
+  return (
+    <>
+      <div>
+        Hi! I&#39;m Anastasia. <br />
+        I&#39;m a motivated frontend developer passionate about creating
+        meaningful digital experiences using modern web technologies. <br />
+        I&#39;m eager to keep learning and growing within a collaborative,
+        professional tech environment.
+      </div>
+      <div>
+        <a href="https://github.com/silvermockingjay">
+          GitHub: @silvermockingjay
+        </a>
+      </div>
+      <div>
+        This app is developed to showcase React: Routing and Hooks Task of
+        <a href="https://rs.school/courses/reactjs">
+          RS School React 2025 Q3 course
+        </a>
+      </div>
+    </>
+  );
+}
