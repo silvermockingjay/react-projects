bbf24b45f1c6fd3bf93beaa55cc3382e5b8141d3
anakdrch <ana.kudrevich@gmail.com>
2025-07-27 18:12:05 +0200

    refactor: migrate CardList component from a class to a function

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import type { JSX } from 'react';
 import { Card } from '../Card/Card';
 import type { Character } from '../../services/interfaces/interfaces';
 import styles from './CardList.module.css';
@@ -7,15 +7,12 @@ export interface CardListProps {
   cards: Character[];
 }
 
-export class CardList extends React.Component<CardListProps> {
-  render() {
-    const { cards } = this.props;
-    return (
-      <div className={styles.cardList}>
-        {cards.map((card, index) => (
-          <Card key={index} {...card} />
-        ))}
-      </div>
-    );
-  }
+export function CardList({ cards }: CardListProps): JSX.Element {
+  return (
+    <div className={styles.cardList}>
+      {cards.map((card, index) => (
+        <Card key={index} {...card} />
+      ))}
+    </div>
+  );
 }
