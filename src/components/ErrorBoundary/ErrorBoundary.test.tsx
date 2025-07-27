6f9382549e2b1ebde2d99dd231787536cd5c6dc8
anakdrch <ana.kudrevich@gmail.com>
2025-07-27 20:14:00 +0200

    test: update ErrorBoundary test after Error Button removal

--- src/components/ErrorBoundary/ErrorBoundary.test.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.test.tsx
@@ -1,37 +1,35 @@
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { ErrorBoundary } from './ErrorBoundary';
-import { App } from '../../App';
-import { userSetUp } from '../../test-utils/test-utils';
 
 describe('ErrorBoundary', () => {
-  test('logs an error to console', async () => {
+  const BuggyComponent = () => {
+    throw new Error('Rendering error');
+  };
+
+  test('logs an error to console', () => {
     const getConsoleErrorSpy = vi
       .spyOn(console, 'error')
       .mockImplementation(() => {});
 
-    const { user } = userSetUp(
+    render(
       <ErrorBoundary>
-        <App />
+        <BuggyComponent />
       </ErrorBoundary>
     );
-    await user.click(screen.getByRole('button', { name: /error button/i }));
+
     expect(getConsoleErrorSpy).toBeCalled();
     getConsoleErrorSpy.mockRestore();
   });
 
-  test('renders fallback UI when error button click triggers error', async () => {
-    const { user } = userSetUp(
+  test('renders fallback UI when rendering error happens', async () => {
+    render(
       <ErrorBoundary>
-        <App />
+        <BuggyComponent />
       </ErrorBoundary>
     );
-    await user.click(screen.getByRole('button', { name: /error button/i }));
     expect(
       await screen.findByText('Something went wrong. Please refresh the page.')
     ).toBeInTheDocument();
-    expect(
-      screen.queryByRole('button', { name: /error button/i })
-    ).not.toBeInTheDocument();
     expect(
       screen.queryByRole('button', { name: /search/i })
     ).not.toBeInTheDocument();
