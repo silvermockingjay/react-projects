eb43a012d7eb3e60a823143c2de62c48413ed220
anakdrch <ana.kudrevich@gmail.com>
2025-07-27 18:26:39 +0200

    refactor: migrate Loader component from a class to a function

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
+import type { JSX } from 'react';
 import styles from './Loader.module.css';
 
-export class Loader extends React.Component {
-  render() {
-    return <div className={styles.pageLoader}></div>;
-  }
+export function Loader(): JSX.Element {
+  return <div className={styles.pageLoader}></div>;
 }
