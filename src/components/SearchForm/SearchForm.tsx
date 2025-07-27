56051a9955ada2f5c44b8627ca763c14c155901e
anakdrch <ana.kudrevich@gmail.com>
2025-07-27 17:36:36 +0200

    refactor: migrate SearchForm component from a class to a function

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { type JSX } from 'react';
 import { CustomButton } from '../CustomButton/CustomButton';
 import { CustomInput } from '../CustomInput/CustomInput';
 import styles from './SearchForm.module.css';
@@ -9,20 +9,21 @@ export interface FormProps {
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
 
-export class SearchForm extends React.Component<FormProps> {
-  render() {
-    const { value, onSubmit, onChange } = this.props;
-    return (
-      <form className={styles.searchForm} onSubmit={onSubmit}>
-        <CustomInput
-          type="text"
-          value={value}
-          customClass={styles.searchInput}
-          placeholder="Search for characters from Rick and Morty, e.g: Rick Sanchez"
-          onChange={onChange}
-        />
-        <CustomButton type="submit" customClass="searchBtn" text="Search" />
-      </form>
-    );
-  }
+export function SearchForm({
+  value,
+  onSubmit,
+  onChange,
+}: FormProps): JSX.Element {
+  return (
+    <form className={styles.searchForm} onSubmit={onSubmit}>
+      <CustomInput
+        type="text"
+        value={value}
+        customClass={styles.searchInput}
+        placeholder="Search for characters from Rick and Morty, e.g: Rick Sanchez"
+        onChange={onChange}
+      />
+      <CustomButton type="submit" customClass="searchBtn" text="Search" />
+    </form>
+  );
 }
