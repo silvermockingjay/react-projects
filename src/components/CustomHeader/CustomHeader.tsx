430c1e5916de1f8e3902a8a5f3570a6de8b0dac4
anakdrch <ana.kudrevich@gmail.com>
2025-07-28 10:48:39 +0200

    feat: use NavLink and add link to Search page

--- src/components/CustomHeader/CustomHeader.tsx
+++ src/components/CustomHeader/CustomHeader.tsx
@@ -1,12 +1,25 @@
-import { Link } from 'react-router';
+import { NavLink } from 'react-router';
 import styles from './CustomHeader.module.css';
 
 export function CustomHeader() {
   return (
     <nav className={styles.nav}>
-      <Link to="/about" className={styles.link}>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? styles.linkActive : styles.link
+        }
+      >
+        Main
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive }) =>
+          isActive ? styles.linkActive : styles.link
+        }
+      >
         About
-      </Link>
+      </NavLink>
     </nav>
   );
 }
