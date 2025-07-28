0f3dd5b74f2c32ecc59b399c440da32b08268279
anakdrch <ana.kudrevich@gmail.com>
2025-07-28 19:40:39 +0200

    feat: add customClass property to CustomSection function component

--- src/components/CustomSection/CustomSection.tsx
+++ src/components/CustomSection/CustomSection.tsx
@@ -1,10 +1,19 @@
 import { type JSX } from 'react';
 import styles from './CustomSection.module.css';
+import classNames from 'classnames';
 
 interface SectionProps {
   children: React.ReactNode;
+  customClass?: string;
 }
 
-export function CustomSection({ children }: SectionProps): JSX.Element {
-  return <section className={styles.pageSection}>{children}</section>;
+export function CustomSection({
+  children,
+  customClass,
+}: SectionProps): JSX.Element {
+  return (
+    <section className={classNames(styles.pageSection, customClass)}>
+      {children}
+    </section>
+  );
 }
