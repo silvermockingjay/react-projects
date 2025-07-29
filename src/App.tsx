057d52ce5465a30ad3231f2015fcd3532de9074f
anakdrch <ana.kudrevich@gmail.com>
2025-07-29 12:10:51 +0200

    fix: correct CardDetails route path

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter(
         ErrorBoundary={ErrorBoundary}
       >
         <Route path="/" element={<SearchPage />}>
-          <Route path=":page/:detailsId" element={<CardDetails />} />
+          <Route path=":page/:details" element={<CardDetails />} />
         </Route>
         <Route path="/about" element={<AboutPage />} />
       </Route>
