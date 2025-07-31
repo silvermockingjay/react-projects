8b1f441988229710d8ca43e4b4477e8efac94d21
anakdrch <ana.kudrevich@gmail.com>
2025-07-31 17:43:02 +0200

    feat: change layout of Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,10 @@
 import type { JSX } from 'react';
 import styles from './Card.module.css';
-import type { Location } from '../../services/interfaces/interfaces';
 
 export interface CardProps {
   id: number;
   image: string;
   name: string;
-  status: string;
-  species: string;
-  gender: string;
-  origin: Location;
-  location: Location;
   openCardDetails: (e: React.MouseEvent<HTMLElement>) => void;
 }
 
@@ -18,11 +12,6 @@ export function Card({
   id,
   image,
   name,
-  status,
-  species,
-  gender,
-  origin,
-  location,
   openCardDetails,
 }: CardProps): JSX.Element {
   return (
@@ -30,27 +19,8 @@ export function Card({
       <div className={styles.itemContainer}>
         <img className={styles.itemImage} src={image} alt={name} />
       </div>
-      <div>
+      <div className={styles.itemContent}>
         <h3 className={styles.itemTitle}>{name}</h3>
-        <div className={styles.itemContent}>
-          <ul>
-            <li>
-              <b>Status: </b> {status}
-            </li>
-            <li>
-              <b>Species: </b> {species}
-            </li>
-            <li>
-              <b>Gender: </b> {gender}
-            </li>
-            <li>
-              <b>Origin: </b> {origin.name}
-            </li>
-            <li>
-              <b>Location: </b> {location.name}
-            </li>
-          </ul>
-        </div>
       </div>
     </div>
   );
