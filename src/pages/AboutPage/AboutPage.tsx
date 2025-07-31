d50040f63a9c2498fe8dcdd04e6bec2485994c40
anakdrch <ana.kudrevich@gmail.com>
2025-07-31 18:33:07 +0200

    feat: make links in AboutPage  open in a new tab

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,6 +1,8 @@
+import styles from './AboutPage.module.css';
+
 export function AboutPage() {
   return (
-    <div data-testId="about-page-content">
+    <div data-testId="about-page-content" className={styles.aboutContainer}>
       <div>
         Hi! I&#39;m Anastasia. <br />
         I&#39;m a motivated frontend developer passionate about creating
@@ -9,13 +11,22 @@ export function AboutPage() {
         professional tech environment.
       </div>
       <div>
-        <a href="https://github.com/silvermockingjay">
-          GitHub: @silvermockingjay
+        GitHub:
+        <a
+          href="https://github.com/silvermockingjay"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @silvermockingjay
         </a>
       </div>
       <div>
         This app is developed to showcase React: Routing and Hooks Task of
-        <a href="https://rs.school/courses/reactjs">
+        <a
+          href="https://rs.school/courses/reactjs"
+          target="_blank"
+          rel="noreferrer"
+        >
           RS School React 2025 Q3 course
         </a>
       </div>
