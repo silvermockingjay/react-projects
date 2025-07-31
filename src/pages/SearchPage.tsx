7e23ffa8c8ace0b6f22d2ccc7f06db0f4959a9c3
anakdrch <ana.kudrevich@gmail.com>
2025-07-31 13:32:39 +0200

    feat: scroll to the CardDetails component

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -125,6 +125,7 @@ export function SearchPage() {
     newSearchParams.set('detailsId', detailsId);
     setSearchParams(newSearchParams);
     navigate(`/details?${newSearchParams.toString()}`);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   let content: React.ReactNode;
