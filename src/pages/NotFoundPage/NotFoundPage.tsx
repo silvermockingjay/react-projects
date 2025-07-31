cd4dc7c86997b6884ebbedc8155a995cf897196b
anakdrch <ana.kudrevich@gmail.com>
2025-07-31 18:39:26 +0200

    style: add styles to NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,11 +1,12 @@
 import { useNavigate } from 'react-router';
 import { CustomButton } from '../../components/CustomButton/CustomButton';
+import styles from './NotFoundPage.module.css';
 
 export function NotFoundPage() {
   const navigate = useNavigate();
   const onClick = () => navigate('/');
   return (
-    <div>
+    <div className={styles.notFoundContainer}>
       <h1>Page not found</h1>
       <p>Sorry, we couldn&#39;t find the page you&#39;re looking for</p>
       <CustomButton
