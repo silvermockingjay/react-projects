12e7a7446452343a3237e20cb052d1ea01b07119
anakdrch <ana.kudrevich@gmail.com>
2025-08-01 14:20:37 +0200

    test: adjust Fallback unit test to check the text content

--- src/components/FallBack/Fallback.test.tsx
+++ src/components/FallBack/Fallback.test.tsx
@@ -4,6 +4,8 @@ import { Fallback } from './Fallback';
 describe('Fallback', () => {
   test('renders successfully with the prop', () => {
     render(<Fallback text="Mock text for fallback" />);
-    expect(screen.getByText('Mock text for fallback')).toBeInTheDocument();
+    expect(screen.getByTestId('fallback-text')).toHaveTextContent(
+      'Mock text for fallback'
+    );
   });
 });
--- src/components/FallBack/Fallback.tsx
+++ src/components/FallBack/Fallback.tsx
@@ -6,5 +6,9 @@ interface FallbackProps {
 }
 
 export function Fallback({ text }: FallbackProps): JSX.Element {
-  return <div className={styles.fallback}>{text}</div>;
+  return (
+    <div className={styles.fallback} data-testid="fallback-text">
+      {text}
+    </div>
+  );
 }
