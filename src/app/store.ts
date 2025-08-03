cd652373d2a3f3ad820d23c76563f26a5bd64eda
anakdrch <ana.kudrevich@gmail.com>
2025-08-04 00:21:35 +0200

    fix: store setup

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cardsReducer from '../features/cards/cardsSlice';
-export const store = configureStore({ reducer: cardsReducer });
+export const store = configureStore({ reducer: { cards: cardsReducer } });
 
 export type AppStore = typeof store;
 export type AppDispatch = typeof store.dispatch;
