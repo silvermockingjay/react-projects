3ebc591470e46bb6ed10b9783614c601fdcfaf7a
anakdrch <ana.kudrevich@gmail.com>
2025-08-04 01:07:05 +0200

    feat: add checkbox to the Card and functionality to keep/update its state in Redux

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,11 @@
 import type { JSX } from 'react';
 import styles from './Card.module.css';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import {
+  cardToggled,
+  selectCheckedCards,
+  type Card,
+} from '../../features/cards/cardsSlice';
 
 export interface CardProps {
   id: number;
@@ -14,6 +20,18 @@ export function Card({
   name,
   openCardDetails,
 }: CardProps): JSX.Element {
+  const dispatch = useAppDispatch();
+  const selectedCards = useAppSelector(selectCheckedCards);
+  const isChecked = selectedCards.some((card) => card.id === id);
+  const card: Card = {
+    id: id,
+    image: image,
+    name: name,
+  };
+  const onChangeToggle = (): void => {
+    dispatch(cardToggled(card));
+  };
+
   return (
     <div
       className={styles.itemCard}
@@ -23,6 +41,12 @@ export function Card({
       aria-label="character card"
     >
       <div className={styles.itemContainer}>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={onChangeToggle}
+          className={styles.checkbox}
+        />
         <img className={styles.itemImage} src={image} alt={name} />
       </div>
       <div className={styles.itemContent}>
