ba084f7d1654cd3d872e328fccd43eddac3c9e0b
anakdrch <ana.kudrevich@gmail.com>
2025-08-04 01:10:01 +0200

    feat: implement customRender to wrap components in Redux <Provider>

--- src/test-utils/test-utils.tsx
+++ src/test-utils/test-utils.tsx
@@ -1,10 +1,16 @@
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import type React from 'react';
+import { Provider } from 'react-redux';
+import { store } from '../app/store';
 
 export function userSetUp(jsx: React.ReactNode) {
   return {
     user: userEvent.setup(),
-    ...render(jsx),
+    ...render(<Provider store={store}>{jsx}</Provider>),
   };
 }
+
+export const customRender = (elem: React.ReactNode) => {
+  render(<Provider store={store}>{elem}</Provider>);
+};
