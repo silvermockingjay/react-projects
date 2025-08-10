3a6d4d49555118e03f678ee4396c35e506cd871a
anakdrch <ana.kudrevich@gmail.com>
2025-08-10 16:36:40 +0200

    feat: implement download functionality in Flyout component

--- src/components/Flyout/Flyout.tsx
+++ src/components/Flyout/Flyout.tsx
@@ -1,13 +1,28 @@
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { allCleared, selectCount } from '../../features/cards/cardsSlice';
+import {
+  allCleared,
+  selectCount,
+  selectCheckedCards,
+} from '../../features/cards/cardsSlice';
+import { downloadCSV } from '../../services/downloadCSV';
 import { CustomButton } from '../CustomButton/CustomButton';
+import { useRef } from 'react';
 import styles from './Flyout.module.css';
 
 export function Flyout() {
   const dispatch = useAppDispatch();
   const selectedCardsCount = useAppSelector(selectCount);
+  const selectedCards = useAppSelector(selectCheckedCards);
   const unselectAll = (): void => {
     dispatch(allCleared());
+  };
+  const linkRef = useRef<HTMLAnchorElement>(null);
+  const download = (): void => {
+    downloadCSV({
+      data: selectedCards,
+      count: selectedCardsCount,
+      ref: linkRef,
+    });
   };
   const flyoutClass = selectedCardsCount ? styles.flyout : styles.hide;
   return (
@@ -19,7 +34,12 @@ export function Flyout() {
           customClass={styles.unselectBtn}
           onClick={unselectAll}
         />
-        <CustomButton text="Download" customClass={styles.downloadBtn} />
+        <CustomButton
+          text="Download"
+          customClass={styles.downloadBtn}
+          onClick={download}
+        />
+        <a ref={linkRef} className={styles.hide}></a>
       </div>
     </div>
   );
