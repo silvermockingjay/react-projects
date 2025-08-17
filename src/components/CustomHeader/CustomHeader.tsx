4b3823329ec0bf84aafa05de2d0947806fbe20b4
anakdrch <ana.kudrevich@gmail.com>
2025-08-17 23:17:44 +0200

    feat: use Link from navigation and add translation to CustomHeader

--- src/components/CustomHeader/CustomHeader.tsx
+++ src/components/CustomHeader/CustomHeader.tsx
@@ -1,25 +1,27 @@
-import { NavLink } from 'react-router';
+'use client';
+
+import { usePathname } from 'next/navigation';
 import styles from './CustomHeader.module.css';
+import { useTranslations } from 'next-intl';
+import { Link } from '../../i18n/navigation';
 
 export function CustomHeader() {
+  const pathname = usePathname();
+  const t = useTranslations();
   return (
     <nav className={styles.nav}>
-      <NavLink
-        to="/"
-        className={({ isActive }) =>
-          isActive ? styles.linkActive : styles.link
-        }
+      <Link
+        href="/"
+        className={pathname === '/' ? styles.linkActive : styles.link}
       >
-        Main
-      </NavLink>
-      <NavLink
-        to="/about"
-        className={({ isActive }) =>
-          isActive ? styles.linkActive : styles.link
-        }
+        {t('main')}
+      </Link>
+      <Link
+        href="/about"
+        className={pathname === '/about' ? styles.linkActive : styles.link}
       >
-        About
-      </NavLink>
+        {t('about')}
+      </Link>
     </nav>
   );
 }
