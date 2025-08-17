09c1acdcf57c59df9b4ba9473b0f347bc620b17d
anakdrch <ana.kudrevich@gmail.com>
2025-08-17 23:05:25 +0200

    feat: pass configuration from i18n/request.ts to Client Components via NextIntlClientProvider

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,22 +1,33 @@
 import { Provider } from 'react-redux';
-import { store } from './store';
-import { CustomHeader } from '../components/CustomHeader/CustomHeader';
-import { CustomMain } from '../components/CustomMain/CustomMain';
-import { ErrorBoundary } from '../components/ErrorBoundary/ErrorBoundary';
+import { store } from '../store';
+import { CustomHeader } from '../../components/CustomHeader/CustomHeader';
+import { CustomMain } from '../../components/CustomMain/CustomMain';
+import { ErrorBoundary } from '../../components/ErrorBoundary/ErrorBoundary';
+import { NextIntlClientProvider, hasLocale } from 'next-intl';
+import { notFound } from 'next/navigation';
+import { routing } from '../../i18n/routing';
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: Promise<{ locale: string }>;
 }) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body>
         <Provider store={store}>
-          <ErrorBoundary>
-            <CustomHeader />
-            <CustomMain>{children}</CustomMain>
-          </ErrorBoundary>
+          <NextIntlClientProvider>
+            <ErrorBoundary>
+              <CustomHeader />
+              <CustomMain>{children}</CustomMain>
+            </ErrorBoundary>
+          </NextIntlClientProvider>
         </Provider>
       </body>
     </html>
