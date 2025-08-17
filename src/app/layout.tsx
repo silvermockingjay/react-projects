8a15da318354ba000bc2815926dd2378cfdefcce
anakdrch <ana.kudrevich@gmail.com>
2025-08-17 19:03:51 +0200

    feat: add ErrorBoundary into rooyt layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import { CustomHeader } from '../components/CustomHeader/CustomHeader';
 import { CustomMain } from '../components/CustomMain/CustomMain';
+import { ErrorBoundary } from '../components/ErrorBoundary/ErrorBoundary';
 
 export default function RootLayout({
   children,
@@ -12,8 +13,10 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Provider store={store}>
-          <CustomHeader />
-          <CustomMain>{children}</CustomMain>
+          <ErrorBoundary>
+            <CustomHeader />
+            <CustomMain>{children}</CustomMain>
+          </ErrorBoundary>
         </Provider>
       </body>
     </html>
