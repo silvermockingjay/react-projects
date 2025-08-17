7bffd173d816413befe2254997ccfdfc1f805f5a
anakdrch <ana.kudrevich@gmail.com>
2025-08-17 22:29:03 +0200

    feat:  setup createNextIntlPlugin

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: './dist',
 };
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
