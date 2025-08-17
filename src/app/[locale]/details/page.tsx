98b1a453abf7ee8584b416d0496b229ea0f9a4a4
anakdrch <ana.kudrevich@gmail.com>
2025-08-17 22:50:58 +0200

    feat: implement App Router and move all files that need translation under [locale] folder

---
+++ src/app/[locale]/details/page.tsx
@@ -0,0 +1,5 @@
+import { CardDetails } from '../../components/CardDetails/CardDetails';
+
+export default function Page() {
+  return <CardDetails />;
+}
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,5 @@
+import { SearchPage } from '../pages/SearchPage/SearchPage';
+
+export default function Page() {
+  return <SearchPage />;
+}
