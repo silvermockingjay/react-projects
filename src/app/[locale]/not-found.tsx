817c9e005fceb77faa6c52cde6923767085d4cf6
anakdrch <ana.kudrevich@gmail.com>
2025-08-18 02:06:53 +0200

    fix: fix import of components to loading.tsx and not-found.tsx

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,4 +1,4 @@
-import { NotFoundPage } from '../pages/NotFoundPage/NotFoundPage';
+import { NotFoundPage } from '../../pages/NotFoundPage/NotFoundPage';
 
 export default function NotFound() {
   return <NotFoundPage />;
