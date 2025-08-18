76c62cd29298259fd201510b4f926021d75a4aa8
anakdrch <ana.kudrevich@gmail.com>
2025-08-18 04:45:32 +0200

    refactor: move redux provider in separate component and mark as a client one

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,11 +1,12 @@
-import { Provider } from 'react-redux';
-import { store } from '../store';
 import { CustomHeader } from '../../components/CustomHeader/CustomHeader';
 import { CustomMain } from '../../components/CustomMain/CustomMain';
 import { ErrorBoundary } from '../../components/ErrorBoundary/ErrorBoundary';
-import { NextIntlClientProvider, hasLocale } from 'next-intl';
+import { hasLocale } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { routing } from '../../i18n/routing';
+import { ReduxProvider } from '../providers/ReduxProvider';
+import { NextIntlClientProvider } from 'next-intl';
+import '../styles/global.css';
 
 export default async function RootLayout({
   children,
@@ -18,17 +19,18 @@ export default async function RootLayout({
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
+
   return (
     <html lang={locale}>
       <body>
-        <Provider store={store}>
+        <ReduxProvider>
           <NextIntlClientProvider>
             <ErrorBoundary>
               <CustomHeader />
               <CustomMain>{children}</CustomMain>
             </ErrorBoundary>
           </NextIntlClientProvider>
-        </Provider>
+        </ReduxProvider>
       </body>
     </html>
   );
