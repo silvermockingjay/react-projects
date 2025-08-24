47fd401171acc9c366812a418b9ad4c710b0e5d5
anakdrch <ana.kudrevich@gmail.com>
2025-08-24 21:48:06 +0200

    feat: implement formsSlice

---
+++ src/app/features/formsSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { RootState } from '../store';
+
+export interface Form {
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+  gender: string;
+  acceptTerms: boolean;
+  picture: string;
+  country: string;
+  createdAt: string;
+}
+
+const initialState: Form[] = [];
+
+const formsSlice = createSlice({
+  name: 'forms',
+  initialState,
+  reducers: {
+    dataSubmitted(state, action: PayloadAction<Form>) {
+      state.push(action.payload);
+    },
+  },
+});
+
+export const { dataSubmitted } = formsSlice.actions;
+export const selectFormsData = (state: RootState) => state.forms;
+export default formsSlice.reducer;
