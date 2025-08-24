ed3994a5c88cd3d96a9b34f1018185c902943cce
anakdrch <ana.kudrevich@gmail.com>
2025-08-25 01:48:29 +0200

    feat: implement covertToBase64 function

---
+++ src/utils/convertToBase64.ts
@@ -0,0 +1,14 @@
+export function convertToBase64(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      if (typeof reader.result === 'string') {
+        resolve(reader.result);
+      } else {
+        reject(new Error('File could not be read'));
+      }
+    };
+    reader.onerror = (error) => reject(error);
+  });
+}
