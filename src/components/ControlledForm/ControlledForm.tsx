1db3941757fddbf08ccf852c49877964805b8f32
anakdrch <ana.kudrevich@gmail.com>
2025-08-25 03:45:53 +0200

    refactor: add visual blocks in form

--- src/components/ControlledForm/ControlledForm.tsx
+++ src/components/ControlledForm/ControlledForm.tsx
@@ -120,87 +120,116 @@ export function ControlledForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="name">Name</label>
-      <input type="text" {...register('name')} id="name" />
-      {errors.name && (
-        <p className={styles.formErrors}>{errors.name.message}</p>
-      )}
-      <label htmlFor="age">Age</label>
-      <input type="number" {...register('age')} id="age" />
-      {errors.age && <p className={styles.formErrors}>{errors.age.message}</p>}
-      <label htmlFor="email">Email</label>
-      <input type="email" {...register('email')} id="email" />
-      {errors.email && (
-        <p className={styles.formErrors}>{errors.email.message}</p>
-      )}
-      <label htmlFor="password">Password</label>
-      <input type="password" {...register('password')} id="password" />
-      {errors.password && (
-        <p className={styles.formErrors}>{errors.password.message}</p>
-      )}
-      <label htmlFor="confirmPassword">Confirm Password</label>
-      <input
-        type="password"
-        {...register('confirmPassword')}
-        id="confirmPassword"
-      />
-      {errors.confirmPassword && (
-        <p className={styles.formErrors}>{errors.confirmPassword.message}</p>
-      )}
-      <div>
-        <p>Gender</p>
-        <label htmlFor="male">Male</label>
-        <input type="radio" value="Male" {...register('gender')} id="male" />
-        <label htmlFor="male">Female</label>
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.formContainer}>
+      <div className={styles.formField}>
+        <label htmlFor="name">Name</label>
+        <input type="text" {...register('name')} id="name" autoComplete="on" />
+        {errors.name && (
+          <p className={styles.formErrors}>{errors.name.message}</p>
+        )}
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="age">Age</label>
+        <input type="text" {...register('age')} id="age" autoComplete="on" />
+        {errors.age && (
+          <p className={styles.formErrors}>{errors.age.message}</p>
+        )}
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="email">Email</label>
         <input
-          type="radio"
-          value="Female"
-          {...register('gender')}
-          id="female"
+          type="email"
+          {...register('email')}
+          id="email"
+          autoComplete="on"
         />
+        {errors.email && (
+          <p className={styles.formErrors}>{errors.email.message}</p>
+        )}
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="password">Password</label>
+        <input type="password" {...register('password')} id="password" />
+        {errors.password && (
+          <p className={styles.formErrors}>{errors.password.message}</p>
+        )}
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          type="password"
+          {...register('confirmPassword')}
+          id="confirmPassword"
+        />
+        {errors.confirmPassword && (
+          <p className={styles.formErrors}>{errors.confirmPassword.message}</p>
+        )}
+      </div>
+      <div className={styles.formField}>
+        <p>Gender</p>
+        <div className={styles.formFieldInline}>
+          <label htmlFor="male">Male</label>
+          <input type="radio" value="Male" {...register('gender')} id="male" />
+          <label htmlFor="male">Female</label>
+          <input
+            type="radio"
+            value="Female"
+            {...register('gender')}
+            id="female"
+          />
+        </div>
         {errors.gender && (
           <p className={styles.formErrors}>{errors.gender.message}</p>
         )}
       </div>
-      <label htmlFor="agreement">I accept T&C agreement</label>
-      <input type="checkbox" {...register('acceptTerms')} id="agreement" />
-      {errors.acceptTerms && (
-        <p className={styles.formErrors}>{errors.acceptTerms.message}</p>
-      )}
-      <label htmlFor="country">Country</label>
-      <input
-        list="countries"
-        {...register('country')}
-        id="country"
-        placeholder="Select a country"
-      />
-      <datalist id="countries">
-        {myCountries.map((country) => (
-          <option key={country} value={country}>
-            {country}
-          </option>
-        ))}
-      </datalist>
-      {errors.country && (
-        <p className={styles.country}>{errors.country.message}</p>
-      )}
-      <label htmlFor="picture">Upload a picture</label>
-      <input
-        type="file"
-        accept="image/jpeg, image/jpg, image/png"
-        {...register('picture')}
-        id="picture"
-        onChange={(e) => {
-          const file = e.target.files?.[0];
-          if (file) setFile(file);
-        }}
-      />
-      <button>Upload picture</button>
-      {errors.picture && (
-        <p className={styles.formErrors}>{errors.picture.message}</p>
-      )}
-      <button type="submit" disabled={!isValid}>
+      <div className={styles.formField}>
+        <div className={styles.formFieldInline}>
+          <label htmlFor="agreement">I accept T&C agreement</label>
+          <input type="checkbox" {...register('acceptTerms')} id="agreement" />
+        </div>
+        {errors.acceptTerms && (
+          <p className={styles.formErrors}>{errors.acceptTerms.message}</p>
+        )}
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="country">Country</label>
+        <input
+          list="countries"
+          {...register('country')}
+          id="country"
+          placeholder="Select a country"
+        />
+        <datalist id="countries">
+          {myCountries.map((country) => (
+            <option key={country} value={country}>
+              {country}
+            </option>
+          ))}
+        </datalist>
+        {errors.country && (
+          <p className={styles.country}>{errors.country.message}</p>
+        )}
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="picture">Upload a picture</label>
+        <div className={styles.formFieldInline}>
+          <input
+            type="file"
+            accept="image/jpeg, image/jpg, image/png"
+            {...register('picture')}
+            id="picture"
+            onChange={(e) => {
+              const file = e.target.files?.[0];
+              if (file) setFile(file);
+            }}
+          />
+        </div>
+        <button className={styles.uploadFileBtn}>Upload picture</button>
+        {errors.picture && (
+          <p className={styles.formErrors}>{errors.picture.message}</p>
+        )}
+      </div>
+      <button className={styles.submitBtn} type="submit" disabled={!isValid}>
         Submit
       </button>
     </form>
