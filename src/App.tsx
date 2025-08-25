6c6ee27337876023b8bcd4686c8c979ed1fe5324
anakdrch <ana.kudrevich@gmail.com>
2025-08-25 03:08:09 +0200

    feat: implement App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import { MainPage } from './pages/MainPage';
+
 export default function App() {
-  return <div>Main page</div>;
+  return (
+    <div>
+      <Provider store={store}>
+        <MainPage />
+      </Provider>
+    </div>
+  );
 }
