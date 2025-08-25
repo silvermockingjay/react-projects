b173f507cfe80fbb7672e66d0a23244df1e8df50
anakdrch <ana.kudrevich@gmail.com>
2025-08-25 03:46:52 +0200

    style: add styles to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { MainPage } from './pages/MainPage';
+import './App.css';
 
 export default function App() {
   return (
