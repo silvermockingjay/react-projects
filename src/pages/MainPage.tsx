a7c188ad7dfb56d7b33d1d5dd47991dd0e1231d6
anakdrch <ana.kudrevich@gmail.com>
2025-08-27 16:14:55 +0200

    refactor: remove unnecessary useEffect from MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,6 @@
-import { useEffect } from 'react';
-import { useAppDispatch } from '../app/hooks';
-import { countriesAdded } from '../app/features/countriesSlice';
-import { countries } from '..//utils/countries';
 import { RHFForm } from '../components/RHFForm/RHFForm';
 
 export function MainPage() {
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(countriesAdded(countries));
-  }, [dispatch]);
   return (
     <main>
       <RHFForm />
